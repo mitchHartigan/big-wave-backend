ec2813380a26e199c188fa683e4697e3d37cb634
Mitchell Hartigan <mitch.hartigan@gmail.com>
2021-04-23 12:12:28 -0700

    feat: added serverless deploy pipeline

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
+"use strict";
+
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-const e = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
+
+const AWS = require("aws-sdk");
+const config = require("./_config");
+const { response } = require("express");
+AWS.config.update(config);
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "bigwave.server@gmail.com",
-    pass: "$bigwaveserver",
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: "bigwave.server@gmail.com",
+//     pass: "$bigwaveserver",
+//   },
+// });
 
-const mailOptions = {
-  from: "bigwave.server@gmail.com",
-  to: "mitch.hartigan@gmail.com",
-  subject: "Sending an email using the big wave server!",
-  text: "Wow, that was easy!",
-};
+// const mailOptions = {
+//   from: "bigwave.server@gmail.com",
+//   to: "mitch.hartigan@gmail.com",
+//   subject: "Sending an email using the big wave server!",
+//   text: "Wow, that was easy!",
+// };
 
-transporter.sendMail(mailOptions, (err, info) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("email sent!" + info.response);
-  }
-});
+// transporter.sendMail(mailOptions, (err, info) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("email sent!" + info.response);
+//   }
+// });
 
 app.get("/", (req, res) => {
-  res.json({ ayy: "lmao" });
+  res.send("ayy lmao");
   res.end();
 });
 
-app.listen(8080, () => {
-  console.log("server running successfully");
+app.post("/study", (req, res) => {});
+
+app.post("/contact", (req, res) => {
+  const { name, email, phone, message } = req.body;
+
+  if (name && email && message) {
+    console.log("endpoint hit!");
+  }
 });
+
+// app.listen(8080, () => {
+//   console.log("server running successfully");
+// });
+
+module.exports = app;
