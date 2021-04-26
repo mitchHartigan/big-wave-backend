2496b756cceba9988471be0160d8950efcde0992
Mitch Hartigan <mitch.hartigan@gmail.com>
2021-04-25 19:04:13 -0700

    fix: updated email subject

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ app.post("/contact", (req, res) => {
     const mailOptions = {
       from: "bigwave.server@gmail.com",
       to: "mitch.hartigan@gmail.com",
-      subject: "Big Wave - New message from contact form.",
+      subject:
+        "[bigwavewater.com Automated Message] - New message from contact form.",
       html: `
         <h4>Name: ${name}</h4>
         <h4>Email: ${email}</h4>
@@ -52,20 +53,43 @@ app.post("/contact", (req, res) => {
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
         console.log(err);
-        res.status(500).send("Sorry - we had an internal server error.");
+        res.status(500).json("Sorry - we had an internal server error.");
       } else {
         console.log("email sent!" + info.response);
-        res.status(200).send("Email sent!");
+        res.status(200).json("Email sent!");
       }
     });
   }
 });
 
 app.post("/casestudy", (req, res) => {
-  const { name, email, phone, message } = req.body;
+  const { name, email, phone, message, study } = req.body;
 
-  if (name && email && message) {
-    console.log("endpoint hit!");
+  if (name && email && message && study) {
+    const mailOptions = {
+      from: "bigwave.server@gmail.com",
+      to: "mitch.hartigan@gmail.com",
+      subject:
+        "[bigwavewater.com Automated Message] - New download request from case study form.",
+      html: `
+        <h4>Name: ${name}</h4>
+        <h4>Email: ${email}</h4>
+        <h4>Phone: ${phone}</h4>
+        <h4>Study requested: ${study}</h4>
+        <h4>Message:</h4> <p>${message}</p>
+        <br></br>
+        <p>(This message was generated automatically from the case study download form on bigwavewater.com)`,
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json("Sorry - we had an internal server error.");
+      } else {
+        console.log("email sent!" + info.response);
+        res.status(200).json("Email sent!");
+      }
+    });
   }
 });
 
