2146e6c00561f12abe98c158d23a5a0a2319e66b
Mitch Hartigan <mitch.hartigan@gmail.com>
2021-04-25 17:55:42 -0700

    feat: send email on POST

--- app.js
+++ app.js
@@ -19,52 +19,54 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: "bigwave.server@gmail.com",
-//     pass: "$bigwaveserver",
-//   },
-// });
-
-// const mailOptions = {
-//   from: "bigwave.server@gmail.com",
-//   to: "mitch.hartigan@gmail.com",
-//   subject: "Sending an email using the big wave server!",
-//   text: "Wow, that was easy!",
-// };
-
-// transporter.sendMail(mailOptions, (err, info) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("email sent!" + info.response);
-//   }
-// });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "bigwave.server@gmail.com",
+    pass: "$bigwaveserver",
+  },
+});
 
 app.get("/", (req, res) => {
-  res.send("ayy lmao");
+  res.send("Big wave email server running sucessfully.");
   res.end();
 });
 
-app.post("/study", (req, res) => {
+app.post("/contact", (req, res) => {
   const { name, email, phone, message } = req.body;
+
   if (name && email && phone) {
-    res.send("here is your study good sir");
-    res.end();
+    const mailOptions = {
+      from: "bigwave.server@gmail.com",
+      to: "mitch.hartigan@gmail.com",
+      subject: "Big Wave - New message from contact form.",
+      html: `
+        <h4>Name: ${name}</h4>
+        <h4>Email: ${email}</h4>
+        <h4>Phone: ${phone}</h4>
+        <h4>Message:</h4> <p>${message}</p>
+        <br></br>
+        <p>(This message was generated automatically from the contact form on bigwavewater.com)`,
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Sorry - we had an internal server error.");
+      } else {
+        console.log("email sent!" + info.response);
+        res.status(200).send("Email sent!");
+      }
+    });
   }
 });
 
-app.post("/contact", (req, res) => {
+app.post("/casestudy", (req, res) => {
   const { name, email, phone, message } = req.body;
 
   if (name && email && message) {
     console.log("endpoint hit!");
   }
 });
-
-// app.listen(8080, () => {
-//   console.log("server running successfully");
-// });
 
 module.exports = app;
